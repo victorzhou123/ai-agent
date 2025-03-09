c7a3137217e4e3ebd74a620c6c965deae219cfa7
victorzhou123 <victorzhoux@163.com>
2025-03-09 23:03:36 +0800

    feat: gin server

--- server/gin.go
+++ server/gin.go
@@ -1,2 +1,41 @@
 package server
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/victorzhou123/ai-agent/config"
+)
+
+const BasePath = "/api"
+
+func StartWebServer(cfg *config.Config) error {
+	engine := gin.New()
+	engine.Use(gin.Recovery())
+
+	engine.UseRawPath = true
+
+	if err := setRouters(engine, cfg); err != nil {
+		return err
+	}
+
+	server := &http.Server{
+		Addr:              fmt.Sprintf(":%d", cfg.Server.Port),
+		Handler:           engine,
+		ReadTimeout:       time.Duration(cfg.Server.ReadTimeout) * time.Millisecond,
+		ReadHeaderTimeout: time.Duration(cfg.Server.ReadHeaderTimeout) * time.Millisecond,
+	}
+
+	return server.ListenAndServe()
+}
+
+func setRouters(engine *gin.Engine, cfg *config.Config) error {
+	// abstract
+
+	// polish
+
+	return nil
+}
